6216390932a212e68fd5d3b3bf5b883ff2c51490
Muharrem <boztepe.muharrem@gmail.com>
2022-03-11 10:41:06 +0300

    9th commit

---
+++ hummingbot/connector/exchange/btcturk/btcturk_constants.py
@@ -0,0 +1,99 @@
+from hummingbot.core.api_throttler.data_types import LinkedLimitWeightPair, RateLimit
+from hummingbot.core.data_type.in_flight_order import OrderState
+
+# BTCTURK NoIdea
+HBOT_ORDER_ID_PREFIX = "x-XEKWYICX"
+
+# Base URL
+REST_URL = "https://api.btcturk.com/api/"
+WSS_URL = "wss://ws-feed-pro.btcturk.com/"
+
+#
+PUBLIC_API_VERSION = "v2"
+PRIVATE_API_VERSION = "v1"
+
+# Public API endpoints
+TICKER_PRICE_CHANGE_PATH_URL = "/ticker/currency"
+EXCHANGE_INFO_PATH_URL = "/server/exchangeInfo"
+# PING_PATH_URL = "/ping"
+# SNAPSHOT_PATH_URL = "/depth"
+# SERVER_TIME_PATH_URL = "/time"
+PUBLIC_ORDERBOOK_PATH = "/orderbook"
+PUBLIC_TRADE_PATH = "/trades"
+# Private API endpoints
+ACCOUNTS_PATH_URL = "/users/balances"
+MY_TRADES_PATH_URL = "/users/transactions/trade"
+OPEN_ORDER_PATH_URL = "/openOrders"
+ALL_ORDER_PATH_URL = "/allOrders"
+ORDER_PATH = "/order"
+# BINANCE_USER_STREAM_PATH_URL = "/userDataStream"
+# WS_HEARTBEAT_TIME_INTERVAL = 30
+
+# Binance params
+
+SIDE_BUY = "buy"
+SIDE_SELL = "sell"
+
+"""TIME_IN_FORCE_GTC = "GTC"  # Good till cancelled
+TIME_IN_FORCE_IOC = "IOC"  # Immediate or cancel
+TIME_IN_FORCE_FOK = "FOK"  # Fill or kill
+"""
+# Rate Limit Type
+REQUEST_WEIGHT = "REQUEST_WEIGHT"
+ORDERS = "ORDERS"
+ORDERS_24HR = "ORDERS_24HR"
+
+# Rate Limit time intervals
+ONE_MINUTE = 60
+ONE_SECOND = 1
+ONE_DAY = 86400
+
+MAX_REQUEST = 5000
+
+# Order States
+ORDER_STATE = {
+    "PENDING": OrderState.PENDING_CREATE,
+    "NEW": OrderState.OPEN,
+    "FILLED": OrderState.FILLED,
+    "PARTIALLY_FILLED": OrderState.PARTIALLY_FILLED,
+    "PENDING_CANCEL": OrderState.OPEN,
+    "CANCELED": OrderState.CANCELLED,
+    "REJECTED": OrderState.FAILED,
+    "EXPIRED": OrderState.FAILED,
+}
+
+# Websocket event types
+DIFF_EVENT_TYPE = "depthUpdate"
+TRADE_EVENT_TYPE = "trade"
+
+RATE_LIMITS = [
+    # Pools
+    RateLimit(limit_id=REQUEST_WEIGHT, limit=1200, time_interval=ONE_MINUTE),
+    RateLimit(limit_id=ORDERS, limit=10, time_interval=ONE_SECOND),
+    RateLimit(limit_id=ORDERS_24HR, limit=100000, time_interval=ONE_DAY),
+    # Weighted Limits
+    RateLimit(
+        limit_id=TICKER_PRICE_CHANGE_PATH_URL,
+        limit=MAX_REQUEST,
+        time_interval=ONE_MINUTE,
+        linked_limits=[LinkedLimitWeightPair(REQUEST_WEIGHT, 40)],
+    ),
+    RateLimit(
+        limit_id=EXCHANGE_INFO_PATH_URL,
+        limit=MAX_REQUEST,
+        time_interval=ONE_MINUTE,
+        linked_limits=[(LinkedLimitWeightPair(REQUEST_WEIGHT, 10))],
+    ),
+    RateLimit(
+        limit_id=ACCOUNTS_PATH_URL,
+        limit=MAX_REQUEST,
+        time_interval=ONE_MINUTE,
+        linked_limits=[LinkedLimitWeightPair(REQUEST_WEIGHT, 10)],
+    ),
+    RateLimit(
+        limit_id=MY_TRADES_PATH_URL,
+        limit=MAX_REQUEST,
+        time_interval=ONE_MINUTE,
+        linked_limits=[LinkedLimitWeightPair(REQUEST_WEIGHT, 10)],
+    ),
+]
---
+++ hummingbot/connector/exchange/btcturk/btcturk_utils.py
@@ -0,0 +1,102 @@
+import os
+import socket
+from typing import Any, Dict
+
+import hummingbot.connector.exchange.btcturk.btcturk_constants as CONSTANTS
+
+from hummingbot.client.config.config_methods import using_exchange
+from hummingbot.client.config.config_var import ConfigVar
+from hummingbot.core.utils.tracking_nonce import get_tracking_nonce
+
+
+CENTRALIZED = True
+EXAMPLE_PAIR = "BTC-USDT"
+DEFAULT_FEES = [0.05, 0.09]
+
+
+def get_new_client_order_id(is_buy: bool, trading_pair: str) -> str:
+    """
+    Creates a client order id for a new order
+    :param is_buy: True if the order is a buy order, False otherwise
+    :param trading_pair: the trading pair the order will be operating with
+    :return: an identifier for the new order to be used in the client
+    """
+    side = "B" if is_buy else "S"
+    symbols = trading_pair.split("-")
+    base = symbols[0].upper()
+    quote = symbols[1].upper()
+    base_str = f"{base[0]}{base[-1]}"
+    quote_str = f"{quote[0]}{quote[-1]}"
+    client_instance_id = hex(abs(hash(f"{socket.gethostname()}{os.getpid()}")))[2:6]
+    return f"{CONSTANTS.HBOT_ORDER_ID_PREFIX}-{side}{base_str}{quote_str}{client_instance_id}{get_tracking_nonce()}"
+
+
+def is_exchange_information_valid(exchange_info: Dict[str, Any]) -> bool:
+    """
+    Verifies if a trading pair is enabled to operate with based on its exchange information
+    :param exchange_info: the exchange information for a trading pair
+    :return: True if the trading pair is enabled, False otherwise
+    """
+    return exchange_info.get("status", None) == "TRADING" and "SPOT" in exchange_info.get("permissions", list())
+
+
+def public_rest_url(path_url: str) -> str:
+    """
+    Creates a full URL for provided public REST endpoint
+    :param path_url: a public REST endpoint
+    :param domain: the Binance domain to connect to ("com" or "us"). The default value is "com"
+    :return: the full URL to the endpoint
+    """
+    return CONSTANTS.REST_URL + CONSTANTS.PUBLIC_API_VERSION + path_url
+
+
+def private_rest_url(path_url: str) -> str:
+    """
+    Creates a full URL for provided private REST endpoint
+    :param path_url: a private REST endpoint
+    :return: the full URL to the endpoint
+    """
+    return CONSTANTS.REST_URL + CONSTANTS.PRIVATE_API_VERSION + path_url
+
+
+KEYS = {
+    "btcturk_api_key": ConfigVar(
+        key="btcturk_api_key",
+        prompt="Enter your Btcturk API key >>> ",
+        required_if=using_exchange("btcturk"),
+        is_secure=True,
+        is_connect_key=True,
+    ),
+    "btcturk_api_secret": ConfigVar(
+        key="btcturk_api_secret",
+        prompt="Enter your Btcturk API secret >>> ",
+        required_if=using_exchange("btcturk"),
+        is_secure=True,
+        is_connect_key=True,
+    ),
+}
+
+"""
+OTHER_DOMAINS = ["binance_us"]
+OTHER_DOMAINS_PARAMETER = {"binance_us": "us"}
+OTHER_DOMAINS_EXAMPLE_PAIR = {"binance_us": "BTC-USDT"}
+OTHER_DOMAINS_DEFAULT_FEES = {"binance_us": [0.1, 0.1]}
+OTHER_DOMAINS_KEYS = {
+    "binance_us": {
+        "binance_us_api_key": ConfigVar(
+            key="binance_us_api_key",
+            prompt="Enter your Binance US API key >>> ",
+            required_if=using_exchange("binance_us"),
+            is_secure=True,
+            is_connect_key=True,
+        ),
+        "binance_us_api_secret": ConfigVar(
+            key="binance_us_api_secret",
+            prompt="Enter your Binance US API secret >>> ",
+            required_if=using_exchange("binance_us"),
+            is_secure=True,
+            is_connect_key=True,
+        ),
+    }
+}
+"""
